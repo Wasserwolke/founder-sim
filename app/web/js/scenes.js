66a814d5036608f2edc5cff3c1336318c3e17769
Wasserwolke <38831318+Wasserwolke@users.noreply.github.com>
2026-08-16 00:56:58 +0200

    Add scene registry

---
+++ app/web/js/scenes.js
@@ -0,0 +1,5 @@
+export const scenes={
+  desk:{asset:"assets/environments/desk_room_starter_night_world.png",placeholder:"HUMBLE BEGINNINGS\nDESK ROOM\nAsset fehlt noch"},
+  storage:{asset:"assets/environments/storage_room_starter_world.png",placeholder:"STARTER STORAGE ROOM\nAsset fehlt noch"},
+  map:{asset:"assets/environments/city_map_starter_world.png",placeholder:"CITY MAP\nAsset fehlt noch"}
+};
