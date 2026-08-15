33ba4468efb7b4f80caea83f2f223de7ca8489e3
Wasserwolke <38831318+Wasserwolke@users.noreply.github.com>
2026-08-16 01:12:35 +0200

    Add public mod API surface

---
+++ app/web/js/modding/mod-api.js
@@ -0,0 +1,20 @@
+export function createModAPI({resources, assets, catalog, i18n}) {
+  const events = new EventTarget();
+  return Object.freeze({
+    apiVersion: 1,
+    resources,
+    assets,
+    catalog,
+    i18n,
+    events: {
+      on(name, handler) {
+        const wrapped = event => handler(event.detail);
+        events.addEventListener(name, wrapped);
+        return () => events.removeEventListener(name, wrapped);
+      },
+      emit(name, detail = {}) {
+        events.dispatchEvent(new CustomEvent(name, {detail}));
+      }
+    }
+  });
+}
